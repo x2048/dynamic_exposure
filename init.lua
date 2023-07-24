7e8903adc05686cbc54f1e9e33002f91c18b2819
x2048 <codeforsmile@gmail.com>
2023-07-25 01:03:05 +0200

    Reduce the dynamic range

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ minetest.register_on_joinplayer(function(player)
 	player:set_lighting({
 		exposure = {
 			luminance_min = -5,
-			luminance_max = -2,
+			luminance_max = -3,
 			exposure_correction = 0,
 			speed_dark_bright = 200,
 			speed_bright_dark = 100,
-			center_weight_power = 1.25
+			center_weight_power = 1
 		}
 	})
 end)
